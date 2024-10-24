96e3a6ff6c7b40cfad2026b38babcdec7b4c3f1b
Ben Croker <ben@putyourlightson.com>
2024-10-24 22:29:18 +0200

    Improvements

--- src/attributes/stores/PokemonEdit.php
+++ src/attributes/stores/PokemonEdit.php
@@ -6,13 +6,13 @@ use Attribute;
 use craft\base\Model;
 
 #[Attribute]
-class PokemonStore extends Model
+class PokemonEdit extends Model
 {
-    /** @var string The name of the Pokemon */
-    public ?string $pokemonName = '';
-
     /** @var int The entry ID of the Pokemon */
     public ?int $entryId = 0;
+
+    /** @var string The name of the Pokemon */
+    public ?string $pokemonName = '';
 
     /** @var bool Whether we are editing the Pokemon */
     public ?bool $editing = false;
