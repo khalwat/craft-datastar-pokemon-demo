338f30b2daba3b8ecfb00933bf8d851792e791f0
Andrew Welch <andrew@nystudio107.com>
2024-10-24 16:20:15 -0400

    refactor: Whitespace

--- src/attributes/stores/PokemonDetail.php
+++ src/attributes/stores/PokemonDetail.php
@@ -11,6 +11,7 @@ class PokemonDetail extends StoreModel
 {
     /** @var ?PokemonEntryType The pokemon entry type data for `entries/save-entry */
     public ?PokemonEntryType $pokemonEntry;
+
     /** @var bool Whether we are editing the Pokemon */
     public ?bool $editing = false;
 }
