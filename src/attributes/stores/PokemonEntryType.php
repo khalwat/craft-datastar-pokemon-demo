6591b7fde20f275dd217abf83523d03c3122b48e
Andrew Welch <andrew@nystudio107.com>
2024-10-24 13:17:35 -0400

    refactor: Extend `Model` to get traversable for free, add default values

--- src/attributes/stores/PokemonEntryType.php
+++ src/attributes/stores/PokemonEntryType.php
@@ -3,31 +3,32 @@
 namespace attributes\stores;
 
 use Attribute;
+use craft\base\Model;
 
 #[Attribute]
-class PokemonEntryType
+class PokemonEntryType extends Model
 {
     /** @var int The entry ID */
-    public int $entryId;
+    public ?int $entryId = 0;
 
     /** @var int The Pokedex ID of the Pokemon */
-    public int $pokemonId;
+    public ?int $pokemonId = 0;
 
     /** @var string The name of the Pokemon */
-    public string $pokemonName;
+    public ?string $pokemonName = '';
 
     /** @var string The form of the Pokemon */
-    public string $pokemonForm;
+    public ?string $pokemonForm = '';
 
     /** @var string The first type of the Pokemon */
-    public string $type1;
+    public ?string $type1 = '';
 
     /** @var string The second type of the Pokemon */
-    public string $type2;
+    public ?string $type2 = '';
 
     /** @var int The generation of the Pokemon */
-    public int $generation;
+    public ?int $generation = 0;
 
     /** @var bool Whether we are editing the Pokemon */
-    public bool $editing;
+    public ?bool $editing = false;
 }
